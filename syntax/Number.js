caf62b8c9a9c23519c9300390db7d09fa8b3ae60
GiBaekAn <akb0516@ajou.ac.kr>
2021-11-01 21:50:47 +0900

    first commit

---
+++ syntax/Number.js
@@ -0,0 +1,14 @@
+console.log(1+1);
+a = 2;
+console.log(a);
+
+var letter = 'Lorem blah blah blah';
+
+console.log(letter);
+
+
+//URLs
+// http://gibs.com:3000/main?id=HTM&page=1
+// 3000 - port
+// main - root
+// id... - query string
